99a182763585b0881ccecdeaef1b77d9780753ae
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2024-02-01 02:31:31 +0200

    removed previous changes

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -2,10 +2,10 @@ import STATUS_CODE from "../constants/statusCodes";
 
 const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  // ADDED THIS IF STATEMENT TO FIX BECAUSE I DIDNT SET STATUS CODES CORRECTLY IN CONTROLLERS WHEN THROWING ERRORS
-  if(res.statusCode === STATUS_CODE.OK){
-    res.statusCode = STATUS_CODE.BAD_REQUEST;
-  }
+  // // ADDED THIS IF STATEMENT TO FIX BECAUSE I DIDNT SET STATUS CODES CORRECTLY IN CONTROLLERS WHEN THROWING ERRORS
+  // if(res.statusCode === STATUS_CODE.OK){
+  //   res.statusCode = STATUS_CODE.BAD_REQUEST;
+  // }
   res.setHeader("content-type", "application/json")
   res.status(statusCode).json({ error: error.message });
   
